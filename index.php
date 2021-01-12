431351ea5654361e36fcd1b16242848fb2021cab
Shahab Athar <shahabathar103@gmail.com>
2021-01-12 21:33:13 +0500

    Add index page html nav bar

--- index.php
+++ index.php
@@ -5,13 +5,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Google Map</title>
 </head>
 
 <body>
     <nav>
         <div class="nav-wrapper">
-            <div class="logo"></div>
+            <div class="logo">
+                <a href="">
+                    <img src="https://blog.gisplanning.com/hs-fs/hubfs/GoogleMaps-Icon-alone-1.png?width=600&name=GoogleMaps-Icon-alone-1.png" alt="">
+                    <h1>Google Maps</h1>
+                </a>
+            </div>
             <div class="nav-links">
                 <ul>
                     <li><a href="./map.php">Maps</a></li>
